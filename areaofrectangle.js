5e09134c1163555b5add8f9be4028e88f0a6c2e9
helloworldmeowser <105734247+helloworldmeowser@users.noreply.github.com>
2023-12-17 16:55:14 -0800

    areaofrectangle.js

--- areaofrectangle.js
+++ areaofrectangle.js
@@ -37,3 +37,7 @@ function sayThanks() {
 sayThanks();
 sayThanks();
 sayThanks();
+function sayThanks(name) {
+  console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
+}
+sayThanks ('Cole');
