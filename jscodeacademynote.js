8a357b53fe9dabbc64ad33b3686bdf9aa92c328d
helloworldmeowser <105734247+helloworldmeowser@users.noreply.github.com>
2023-12-28 19:59:32 -0800

    Update jscodeacademynote.js

--- jscodeacademynote.js
+++ jscodeacademynote.js
@@ -83,4 +83,6 @@ const functionName = () => {};
 const functinoName = paramOne => {};
 //Two or More Parameters
 const functinoName = (paramOne, paramTwo) => {};
-
+//Let’s refactor plantNeedsWater() to be a concise body. 
+//Notice that we’ve already converted the if/else statement to a ternary operator to make the code fit on one line.
+const plantNeedsWater = day => day === 'Wednesday' ? true : false;
